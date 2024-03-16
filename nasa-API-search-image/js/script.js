93ec194c1011cd31b4bd888ce257c3b41900d867
Emanuel Overlay <emanueloverlay@outlook.com>
2024-03-16 20:42:48 -0300

    nasa-API-search-image update with modal

--- nasa-API-search-image/js/script.js
+++ nasa-API-search-image/js/script.js
@@ -6,7 +6,7 @@ const FORMBUSCAR = document.getElementById("formBuscar");
 //BTNBUSCAR.addEventListener("click", () => {
 FORMBUSCAR.addEventListener("submit", (event) => {
 
-   event.preventDefault();
+    event.preventDefault();
 
     fetch(API_URL + INPUTBUSCAR.value)
         .then(res => {
@@ -28,20 +28,20 @@ FORMBUSCAR.addEventListener("submit", (event) => {
                         links
                     } = item;
 
-                    const { description, title, date_created } = galaxy;
+                    const { description, title, date_created, nasa_id } = galaxy;
                     let href = "";
 
-                    if (links && links.length > 0){
+                    if (links && links.length > 0) {
                         href = links[0].href;
                     }
 
                     htmlContentToAppend +=
 
                         `  <div class="col-sm p-4">
-                <div class="card" style="width: 18rem;">
+                <div class="card h-100" style="width: 18rem;">
                     <img class="card-img-top" style="height:225px; width: 100%;"src="${href}" />
                     <div class="card-body">
-                        <b class="card-title">${title}</b>
+                        <button class="btn btn-sm btn-primary" onclick="viewInfo('${nasa_id}');" data-bs-toggle="modal" data-bs-target="#detailModal">${title}</button>
                         <div class="card-text" style="overflow-y:scroll; height: 110px;">${description}</div>
                         <p class="card-text">${date_created}</p>
                     </div>
@@ -58,3 +58,41 @@ FORMBUSCAR.addEventListener("submit", (event) => {
             console.log(event);
         });
 });
+
+function viewInfo(id) {
+    const detailModalLabel = document.getElementById('detailModalLabel')
+    const detailModalInfo = document.getElementById('modal-body')
+
+    fetch(API_URL + id)
+        .then(res => {
+            if (res.ok) { return res.json() }
+            else { console.log('Error en el fetch') }
+        })
+        .then(data => {
+            const {
+                collection: { items },
+            } = data;
+
+                const {
+                    data: [galaxy],
+                    links
+                } = items[0];
+
+                const { description, title, date_created } = galaxy;
+                let href = "";
+
+                if (links && links.length > 0) {
+                    href = links[0].href;
+                }
+
+                detailModalLabel.innerHTML = title;
+                detailModalInfo.innerHTML = `
+                <img src="${href}">
+                <br>
+                <br>
+            `
+                detailModalInfo.innerHTML += description;
+
+
+        })
+}
